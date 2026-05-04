a3032ef97e550ddd11098d14230d78292e48d9ae
chanatip1234 <chanatip.auam@bumail.net>
2026-05-04 16:41:42 +0700

    ไม่อยากเเก้กระสุนเเล้วช่วยด้วย

--- Assets/Script/EnemyBullet.cs
+++ Assets/Script/EnemyBullet.cs
@@ -3,23 +3,37 @@ using UnityEngine;
 public class EnemyBullet : MonoBehaviour
 {
     [HideInInspector] public float damage;
- 
+    public float checkRadius = 0.5f; 
+    public LayerMask playerLayer;    
 
+    private void Update()
+    {
+        Collider2D hitPlayer = Physics2D.OverlapCircle(transform.position, checkRadius, playerLayer);
+
+        if (hitPlayer != null)
+        {
+            if (hitPlayer.CompareTag("Player"))
+            {
+                PlayerController player = hitPlayer.GetComponent<PlayerController>();
+                if (player != null)
+                {
+                    player.TakeDamage(damage);
+                    Debug.Log("Direct Hit with Overlap!");
+                }
+                Destroy(gameObject);
+            }
+        }
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player"))
-        {
-            PlayerController player = collision.GetComponent<PlayerController>();
-            if (player != null)
-            {
-                player.TakeDamage(damage);
-            }
-            Destroy(gameObject); 
-        }
-
         if (collision.CompareTag("Ground") || collision.CompareTag("Trap"))
         {
             Destroy(gameObject);
         }
+    }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, checkRadius);
     }
 }
